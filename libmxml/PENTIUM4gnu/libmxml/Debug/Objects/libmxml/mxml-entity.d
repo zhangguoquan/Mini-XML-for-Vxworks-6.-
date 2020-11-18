3d4f89df437401d6f188c811b9b7b8d788f3bd43
zhang guoquan <zhang_guoquan@careri.com>
2020-11-18 19:55:47 +0800

    first init
    Port Mini-xml3.2 to vxworks6.8，this is  a static kernel library example.

---
+++ libmxml/PENTIUM4gnu/libmxml/Debug/Objects/libmxml/mxml-entity.d
@@ -0,0 +1,173 @@
+libmxml/Debug/Objects/libmxml/mxml-entity.o:  \
+ C:/WindRiver/workspace/libmxml/mxml-entity.c \
+  C:/WindRiver/workspace/libmxml/mxml-private.h \
+  C:/WindRiver/workspace/libmxml/mxml-config.h \
+  C:/WindRiver/vxworks-6.8/target/h/stdio.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxANSI.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxCpu.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxArch.h \
+  C:/WindRiver/vxworks-6.8/target/h/arch/i86/archI86.h \
+  C:/WindRiver/vxworks-6.8/target/h/vsbConfig.h \
+  C:/WindRiver/vxworks-6.8/target/lib/h/config/vsbConfig.h \
+  C:/WindRiver/vxworks-6.8/target/lib/h/config/autoconf.h \
+  C:/WindRiver/vxworks-6.8/target/h/arch/i86/archI86Common.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxParams.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxTypesBase.h \
+  c:\windriver\gnu\4.1.2-vxworks-6.8\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.1.2/include/stddef.h \
+  C:/WindRiver/vxworks-6.8/target/h/errno.h \
+  C:/WindRiver/vxworks-6.8/target/h/private/handleLibP.h \
+  C:/WindRiver/vxworks-6.8/target/h/vxWorks.h \
+  C:/WindRiver/vxworks-6.8/target/h/vxWorksCommon.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxTypes.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxANSI.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_ssize_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_cpuset_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_atomic_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_time_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_dev_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_gid_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_ino_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_mode_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_nlink_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_off_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_pid_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_uid_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_blksize_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_blkcnt_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxTypesOld.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/e_vxTypesOld.h \
+  C:/WindRiver/vxworks-6.8/target/h/sys/types.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_pthread_attr_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_size_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_struct__Sched_param.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_struct__Timespec.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_clock_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_key_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_timer_t.h \
+  C:/WindRiver/vxworks-6.8/target/h/base/b_BOOL.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxWind.h \
+  C:/WindRiver/vxworks-6.8/target/h/types/vxWindCommon.h \
+  C:/WindRiver/vxworks-6.8/target/h/tool/gnu/toolMacros.h \
+  C:/WindRiver/vxworks-6.8/target/h/tool/common/private/toolMacrosP.h \
+  C:/WindRiver/vxworks-6.8/target/h/version.h \
+  c:\windriver\gnu\4.1.2-vxworks-6.8\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.1.2/include/stdarg.h \
+  C:/WindRiver/vxworks-6.8/target/h/stdlib.h \
+  C:/WindRiver/vxworks-6.8/target/h/string.h \
+  c:\windriver\gnu\4.1.2-vxworks-6.8\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.1.2/include/stdarg.h \
+  C:/WindRiver/vxworks-6.8/target/h/ctype.h \
+  C:/WindRiver/workspace/libmxml/mxml.h \
+  C:/WindRiver/vxworks-6.8/target/h/errno.h
+
+C:/WindRiver/workspace/libmxml/mxml-private.h:
+
+C:/WindRiver/workspace/libmxml/mxml-config.h:
+
+C:/WindRiver/vxworks-6.8/target/h/stdio.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxANSI.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxCpu.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxArch.h:
+
+C:/WindRiver/vxworks-6.8/target/h/arch/i86/archI86.h:
+
+C:/WindRiver/vxworks-6.8/target/h/vsbConfig.h:
+
+C:/WindRiver/vxworks-6.8/target/lib/h/config/vsbConfig.h:
+
+C:/WindRiver/vxworks-6.8/target/lib/h/config/autoconf.h:
+
+C:/WindRiver/vxworks-6.8/target/h/arch/i86/archI86Common.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxParams.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxTypesBase.h:
+
+c:\windriver\gnu\4.1.2-vxworks-6.8\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.1.2/include/stddef.h:
+
+C:/WindRiver/vxworks-6.8/target/h/errno.h:
+
+C:/WindRiver/vxworks-6.8/target/h/private/handleLibP.h:
+
+C:/WindRiver/vxworks-6.8/target/h/vxWorks.h:
+
+C:/WindRiver/vxworks-6.8/target/h/vxWorksCommon.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxTypes.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxANSI.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_ssize_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_cpuset_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_atomic_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_time_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_dev_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_gid_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_ino_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_mode_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_nlink_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_off_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_pid_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_uid_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_blksize_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_blkcnt_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxTypesOld.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/e_vxTypesOld.h:
+
+C:/WindRiver/vxworks-6.8/target/h/sys/types.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_pthread_attr_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_size_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_struct__Sched_param.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_struct__Timespec.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_clock_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_key_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_timer_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h/base/b_BOOL.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxWind.h:
+
+C:/WindRiver/vxworks-6.8/target/h/types/vxWindCommon.h:
+
+C:/WindRiver/vxworks-6.8/target/h/tool/gnu/toolMacros.h:
+
+C:/WindRiver/vxworks-6.8/target/h/tool/common/private/toolMacrosP.h:
+
+C:/WindRiver/vxworks-6.8/target/h/version.h:
+
+c:\windriver\gnu\4.1.2-vxworks-6.8\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.1.2/include/stdarg.h:
+
+C:/WindRiver/vxworks-6.8/target/h/stdlib.h:
+
+C:/WindRiver/vxworks-6.8/target/h/string.h:
+
+c:\windriver\gnu\4.1.2-vxworks-6.8\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.1.2/include/stdarg.h:
+
+C:/WindRiver/vxworks-6.8/target/h/ctype.h:
+
+C:/WindRiver/workspace/libmxml/mxml.h:
+
+C:/WindRiver/vxworks-6.8/target/h/errno.h:
---
+++ libmxml/SIMNTdiab/libmxml/Debug/Objects/libmxml/mxml-file.d
@@ -0,0 +1,174 @@
+libmxml/Debug/Objects/libmxml/mxml-file.o: \
+  C:/WindRiver/workspace/libmxml/mxml-file.c \
+  C:/WindRiver/vxworks-6.8/target/h\unistd.h \
+  C:/WindRiver/vxworks-6.8/target/h\vxWorks.h \
+  C:/WindRiver/vxworks-6.8/target/h\vxWorksCommon.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxCpu.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxArch.h \
+  C:/WindRiver/vxworks-6.8/target/h\arch/simnt/archSimnt.h \
+  C:/WindRiver/vxworks-6.8/target/h\vsbConfig.h \
+  C:/WindRiver/vxworks-6.8/target/lib/h/config/vsbConfig.h \
+  C:/WindRiver/vxworks-6.8/target/lib/h/config\autoconf.h \
+  C:/WindRiver/vxworks-6.8/target/h\arch/simnt/archSimntCommon.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxParams.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxTypesBase.h \
+  C:\WindRiver\diab\5.8.0.0\include\diab\stddef.h \
+  C:\WindRiver\diab\5.8.0.0\include\diab\xmacros.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxTypes.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxANSI.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_ssize_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_cpuset_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_atomic_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_time_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_dev_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_gid_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_ino_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_mode_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_nlink_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_off_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_pid_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_uid_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_blksize_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_blkcnt_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxTypesOld.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/e_vxTypesOld.h \
+  C:/WindRiver/vxworks-6.8/target/h\sys/types.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_pthread_attr_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_size_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_struct__Sched_param.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_struct__Timespec.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_clock_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_key_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_timer_t.h \
+  C:/WindRiver/vxworks-6.8/target/h\base/b_BOOL.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxWind.h \
+  C:/WindRiver/vxworks-6.8/target/h\types/vxWindCommon.h \
+  C:/WindRiver/vxworks-6.8/target/h\tool/diab/toolMacros.h \
+  C:/WindRiver/vxworks-6.8/target/h\tool/common/private/toolMacrosP.h \
+  C:/WindRiver/vxworks-6.8/target/h\version.h \
+  C:/WindRiver/workspace/libmxml\mxml-private.h \
+  C:/WindRiver/workspace/libmxml\mxml-config.h \
+  C:/WindRiver/vxworks-6.8/target/h\stdio.h \
+  C:/WindRiver/vxworks-6.8/target/h\errno.h \
+  C:/WindRiver/vxworks-6.8/target/h\private/handleLibP.h \
+  C:\WindRiver\diab\5.8.0.0\include\diab\stdarg.h \
+  C:\WindRiver\diab\5.8.0.0\include\diab\va_list.h \
+  C:/WindRiver/vxworks-6.8/target/h\stdlib.h \
+  C:/WindRiver/vxworks-6.8/target/h\string.h \
+  C:/WindRiver/vxworks-6.8/target/h\ctype.h \
+  C:/WindRiver/workspace/libmxml\mxml.h
+
+C:/WindRiver/vxworks-6.8/target/h\unistd.h:
+
+C:/WindRiver/vxworks-6.8/target/h\vxWorks.h:
+
+C:/WindRiver/vxworks-6.8/target/h\vxWorksCommon.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxCpu.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxArch.h:
+
+C:/WindRiver/vxworks-6.8/target/h\arch/simnt/archSimnt.h:
+
+C:/WindRiver/vxworks-6.8/target/h\vsbConfig.h:
+
+C:/WindRiver/vxworks-6.8/target/lib/h/config/vsbConfig.h:
+
+C:/WindRiver/vxworks-6.8/target/lib/h/config\autoconf.h:
+
+C:/WindRiver/vxworks-6.8/target/h\arch/simnt/archSimntCommon.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxParams.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxTypesBase.h:
+
+C:\WindRiver\diab\5.8.0.0\include\diab\stddef.h:
+
+C:\WindRiver\diab\5.8.0.0\include\diab\xmacros.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxTypes.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxANSI.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_ssize_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_cpuset_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_atomic_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_time_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_dev_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_gid_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_ino_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_mode_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_nlink_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_off_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_pid_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_uid_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_blksize_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_blkcnt_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxTypesOld.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/e_vxTypesOld.h:
+
+C:/WindRiver/vxworks-6.8/target/h\sys/types.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_pthread_attr_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_size_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_struct__Sched_param.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_struct__Timespec.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_clock_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_key_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_timer_t.h:
+
+C:/WindRiver/vxworks-6.8/target/h\base/b_BOOL.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxWind.h:
+
+C:/WindRiver/vxworks-6.8/target/h\types/vxWindCommon.h:
+
+C:/WindRiver/vxworks-6.8/target/h\tool/diab/toolMacros.h:
+
+C:/WindRiver/vxworks-6.8/target/h\tool/common/private/toolMacrosP.h:
+
+C:/WindRiver/vxworks-6.8/target/h\version.h:
+
+C:/WindRiver/workspace/libmxml\mxml-private.h:
+
+C:/WindRiver/workspace/libmxml\mxml-config.h:
+
+C:/WindRiver/vxworks-6.8/target/h\stdio.h:
+
+C:/WindRiver/vxworks-6.8/target/h\errno.h:
+
+C:/WindRiver/vxworks-6.8/target/h\private/handleLibP.h:
+
+C:\WindRiver\diab\5.8.0.0\include\diab\stdarg.h:
+
+C:\WindRiver\diab\5.8.0.0\include\diab\va_list.h:
+
+C:/WindRiver/vxworks-6.8/target/h\stdlib.h:
+
+C:/WindRiver/vxworks-6.8/target/h\string.h:
+
+C:/WindRiver/vxworks-6.8/target/h\ctype.h:
+
+C:/WindRiver/workspace/libmxml\mxml.h:
+
